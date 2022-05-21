b58364a69a8c8e4bed14b9bfdabe9dfb7193e40e
edmfiedler <105977010+edmfiedler@users.noreply.github.com>
2022-05-21 14:58:48 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,85 @@
+Put the plugins relocate and scanright into the folder:
+
+/home/smr/mobotware/aurs-plugins
+
+In /home/smr/offline/sim388, add course_line, course_obst, and
+simconfigcourse. Also, in ulmsserver.ini, add the locations of the plugins as:
+
+module load="/home/smr/mobotware/aurs-plugins/scanright/scanright.so.0"
+module load="/home/smr/mobotware/aurs-plugins/relocate/relocate.so.0"
+
+at the bottom.
+
+To run, open a terminal open the terminal on the desktop and do:
+
+cd offline/sim388
+
+Then, start the simulator:
+
+simserver1 simconfigcourse.xml
+
+Open another terminal in the same location (Ctrl+Shift+T) and start the 
+laser server:
+
+ulmsserver
+
+Check if the modules loaded in correctly using:
+
+module list
+
+It should show that scanright and relocate have loaded.
+
+Next, open another terminal and load the script:
+
+mrc -s8000 course_graphplanner
+
+The robot will go to whatever guidemark is specified as rt[0] first (set to
+12), and will then read the gmno of that guidemark in simconfigcourse.xml 
+to decide to which guidemark it is going to navigate to next. 
+
+Based on simconfigcourse.xml, the only path that leads to either 13 or 14
+is as follows:
+
+2->7->11->3->6->12->13
+
+Going to any other guidemark (other than directly to 14) would lead to
+gettting stuck at 1, 4, 5, due to the specified gmno values being read by
+the robot.
+
+The navigation works by knowing the positions it should drive to to see the
+guidemark in the arrays gmx and gmy and the actual position of the guidemark
+in the arrays gx and gy, in order to face the guidemark.
+
+After reading the gmno of guidemark 13 or 14, the robot goes roughly into 
+the position (2,2.25,-1.57) and starts the object detection. It first uses
+the plugin relocate to determine a position facing perpendicular to the 
+right-most side of the shape, in between the left and right-most points of
+the shape that are being seen. It then uses the plugin scanright, to scan
+the right-most point from that position and determines the location and 
+orientation of where to take the next scan from. 
+
+The scans are corners of the shape, but the data has to be treated in 
+order to analyse it:
+1) A mean position of points closer than 0.05 is taken (for when 4 scans
+are taken for the triangle
+2) The order of the points scanned is reversed to obtain positive internal
+angles (not sure if this is always required, in MATLAB this works
+consistently.)
+
+Then, based on internal angles, and side length, the type, position, and
+orientation can be determined. This is shown in the executed code in the 
+terminal e.g. Rectangle 04x0.15 (x,y,ori). The validity of this output
+can be checked using the MATLAB script course_detect in the folder 
+coursedetect.
+
+The script chooses a random pose for a random shape and places it within
+the boundaries. The output in the command window gives the connections of 
+points used to make the shape which can be copy-pasted into course_obst
+such that the same shape can be drawn in the simulator. It then gives the 
+calculated shape classification and pose, and then the actual. The 
+calculation is based on the same principle of what is used in the SMR.
+The actual classification and pose can be compared to what is found in the 
+SMR.
+
+The robot then drives to the checkpoint it is closest to and drives to the
+start position.
---
+++ plugincode/relocate/relocate.cpp
@@ -0,0 +1,227 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Christian Andersen and DTU                      *
+ *   jca@oersted.dtu.dk                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "relocate.h"
+
+#ifdef LIBRARY_OPEN_NEEDED
+
+/**
+ * This function is needed by the server to create a version of this plugin */
+UFunctionBase * createFunc()
+{ // create an object of this type
+  /** replace 'UFunczoneobst' with your class name */
+  return new relocate();
+}
+#endif
+
+bool relocate::setResource(UResBase * resource, bool remove) { // load resource as provided by the server (or other plugins)
+	bool result = true;
+
+	if (resource->isA(UResPoseHist::getOdoPoseID())) { // pointer to server the resource that this plugin can provide too
+		// but as there might be more plugins that can provide the same resource
+		// use the provided
+		if (remove)
+			// the resource is unloaded, so reference must be removed
+			poseHist = NULL;
+		else if (poseHist != (UResPoseHist *) resource)
+			// resource is new or is moved, save the new reference
+			poseHist = (UResPoseHist *) resource;
+		else
+			// reference is not used
+			result = false;
+	}
+
+	// other resource types may be needed by base function.
+	result = UFunctionBase::setResource(resource, remove);
+	return result;
+}
+
+
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+// #define SMRWIDTH 0.4
+bool relocate::handleCommand(UServerInMsg * msg, void * extra)
+{  // handle a plugin command
+  const int MRL = 500;
+  char reply[MRL];
+  bool ask4help;
+  const int MVL = 30;
+  char value[MVL];
+  ULaserData * data;
+  double robotwidth;
+  //
+  int i,j,imax;
+  double r,delta;
+  double minRange; // min range in meter
+  // double minAngle = 0.0; // degrees
+//   double d,robotwidth;
+  double zone[9];
+  // check for parameters - one parameter is tested for - 'help'
+  ask4help = msg->tag.getAttValue("help", value, MVL);
+  if (ask4help)
+  { // create the reply in XML-like (html - like) format
+    sendHelpStart(msg, "relocate");
+    sendText("--- available zoneobst options\n");
+    sendText("help            This message\n");
+    sendText("fake=F          Fake some data 1=random, 2-4 a fake corridor\n");
+    sendText("device=N        Laser device to use (see: SCANGET help)\n");
+    sendText("see also: SCANGET and SCANSET\n");
+    sendHelpDone();
+  }
+  else
+  { // do some action and send a reply
+    data = getScan(msg, (ULaserData*)extra);
+    // 
+    if (data->isValid())
+    {
+    // check if a attribute (parameter) with name width exists and if so get its value
+       bool gotwidth = msg->tag.getAttValue("width", value, MVL);
+       if (gotwidth) {
+        	robotwidth=strtod(value, NULL);   
+       }
+       else {
+        	robotwidth=0.26;
+      }
+      UPose poseAtScan = poseHist->getPoseAtTime(data->getScanTime());
+      // Gets the odometry pose at the time when the laserscan was taken, poseAtScan.x poseAtScan.y poseAtScan.a (x,y,angle)
+      // make analysis for closest measurement
+
+      /*
+      minRange = 1000; // long range in meters
+      imax=data->getRangeCnt();
+      delta=imax/9.0;
+      for (j=0;j<9;j++)
+	 zone[j]=minRange;
+      for(j=0;j<9;j++){
+      for (i = 0+(int)(j*delta); i < (int)((j+1)*delta); i++)
+      { // range are stored as an integer in current units
+	r = data->getRangeMeter(i);
+        if (r >= 0.020)
+        { // less than 20 units is a flag value for URG scanner
+          if (r<zone[j])
+	     zone[j]=r;
+        }
+      }
+      }
+      */
+	
+        imax=data->getRangeCnt();
+
+	double r1_dist, r1_ang, r, r2_dist, r2_ang;
+	// Obtain the detected furthest point from the right of the robot
+	for (i=imax;i>0;i--){
+		r = data->getRangeMeter(i);
+		if (r>=0.02){
+			if (r<=1.5){
+			r1_dist = r;
+			r1_ang = (-(i)*0.36)*3.1416/180;
+			r2_dist = data->getRangeMeter(i-2);
+			r2_ang = (-(i-2)*0.36)*3.1416/180;
+			break;
+			}
+		}
+	}
+
+	double rh_dist, rh_ang;
+	// Obtain the detected furthest point from the left of the robot
+	for (i=0;i<imax;i++){
+		r = data->getRangeMeter(i);
+		if (r>=0.02){
+			if (r<=1.5){
+			rh_dist = r;
+			rh_ang = (-(i)*0.36)*3.1416/180;
+			break;
+			}
+		}
+	}
+
+	double x1, y1, x2, y2, xh, yh;
+	x1 = r1_dist*cos(r1_ang);
+	y1 = r1_dist*sin(r1_ang)-0.26;
+	x2 = r2_dist*cos(r2_ang);
+	y2 = r2_dist*sin(r2_ang)-0.26;
+	xh = rh_dist*cos(rh_ang);
+	yh = rh_dist*sin(rh_ang)-0.26;
+
+	double ang;
+	if (x1>=x2) {
+		ang = atan2(y2-y1,x2-x1)-3.1416/2;
+	} else {
+		ang = atan2(y1-y2,x1-x2)-3.1416/2;
+	} 
+
+	double mx, my;
+	mx = (x1+x2)/2;
+	my = (y1+y2)/2;
+
+	double rx, ry, ra;
+	rx = mx+0.7*cos(ang);
+	ry = my+0.7*sin(ang);
+	ra = ang+3.1416;
+
+	double rxalt, ryalt, raalt;
+	rxalt = mx+0.7*cos(ang+3.1416);
+	ryalt = my+0.7*sin(ang+3.1416);
+	raalt = ang;
+
+	
+	/*if (abs(rxalt) < abs(rx)) {
+		rx = rxalt;
+		ry = ryalt;
+		ra = raalt;
+	}*/
+
+/*
+	double min1_dist, min1_ang;
+	// Obtain the nearest point (closest corner to the robot)
+	for (i=0;i<imax-1;i++){
+		if (data->getRangeMeter(i)>=0.02){
+			if (data->getRangeMeter(i)<data->getRangeMeter(i+1)){
+				min1_dist = data->getRangeMeter(i);
+				min1_ang = (90-i*0.36)*3.14/180;
+				break;
+			}
+		}
+	}
+*/
+
+      /* SMRCL reply format */
+      snprintf(reply, MRL, "<laser l6=\"%g\" l7=\"%g\" l8=\"%g\" />\n", 
+	                  rx,ry,ra);
+      // send this string as the reply to the client
+      sendMsg(msg, reply);
+      // save also as gloabl variable
+      	var_zone->setValued(r1_dist,0);
+	//var_zone->setValued(parky,1);
+    }
+    else
+      sendWarning(msg, "No scandata available");
+  }
+  // return true if the function is handled with a positive result
+  // used if scanpush or push has a count of positive results
+  return true;
+}
+
+void relocate::createBaseVar()
+{ // add also a global variable (global on laser scanner server) with latest data
+  var_zone = addVarA("zone", "0 0 0 0 0 0 0 0 0", "d", "Value of each laser zone. Updated by zoneobst.");
+}
